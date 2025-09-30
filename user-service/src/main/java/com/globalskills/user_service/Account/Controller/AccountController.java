bd481e80ae1cd9bd2b2d462886581a682330401d
QuanDong <quanonepunch@gmail.com>
2025-09-30 10:24:01 +0700

    fix path

--- user-service/src/main/java/com/globalskills/user_service/Account/Controller/AccountController.java
+++ user-service/src/main/java/com/globalskills/user_service/Account/Controller/AccountController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/account")
+@RequestMapping("/account")
 @CrossOrigin("*")
 public class AccountController {
 
