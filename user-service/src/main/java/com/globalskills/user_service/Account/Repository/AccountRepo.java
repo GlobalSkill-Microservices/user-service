a755a0a21a48aed9e5b395bb76b8d63e7c38b375
QuanDong <quanonepunch@gmail.com>
2025-10-04 07:58:28 +0700

    api list account

--- user-service/src/main/java/com/globalskills/user_service/Account/Repository/AccountRepo.java
+++ user-service/src/main/java/com/globalskills/user_service/Account/Repository/AccountRepo.java
@@ -1,6 +1,8 @@
 package com.globalskills.user_service.Account.Repository;
 
 import com.globalskills.user_service.Account.Entity.Account;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +13,6 @@ public interface AccountRepo extends JpaRepository<Account,Long> {
     Optional<Account> findByUsername(String username);
     boolean existsByEmailOrUsername(String email, String username);
     Optional<Account> findByEmail(String email);
+
+    Page<Account> findAllByIsActive(Pageable pageable,boolean isActive);
 }
