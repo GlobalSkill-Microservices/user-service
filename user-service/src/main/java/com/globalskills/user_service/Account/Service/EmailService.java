487faa51e7f0f908ecc159f1de1492de55cd79d9
QuanDong <quanonepunch@gmail.com>
2025-10-17 10:04:11 +0700

    smtp mail by sendgrid

--- user-service/src/main/java/com/globalskills/user_service/Account/Service/EmailService.java
+++ user-service/src/main/java/com/globalskills/user_service/Account/Service/EmailService.java
@@ -2,16 +2,11 @@ package com.globalskills.user_service.Account.Service;
 
 import com.globalskills.user_service.Account.Dto.EmailDto;
 import com.globalskills.user_service.Account.Exception.EmailException;
-import com.postmarkapp.postmark.Postmark;
-import com.postmarkapp.postmark.client.ApiClient;
-import com.postmarkapp.postmark.client.data.model.message.Message;
-import com.postmarkapp.postmark.client.data.model.message.MessageResponse;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -27,14 +22,6 @@ public class EmailService {
     @Autowired
     JavaMailSender javaMailSender;
 
-    @Value("${postmark.api.token}")
-    private String postmarkApiToken;
-
-
-    @Value("${postmark.sender.email}")
-    private String senderEmail;
-
-
     @Autowired
     TemplateEngine templateEngine;
 
@@ -46,22 +33,16 @@ public class EmailService {
             context.setVariable("link", emailDetail.getLink());
             context.setVariable("date", emailDetail.getCreatedDate());
             String template = templateEngine.process("EmailVerify", context);
-            ApiClient client = Postmark.getApiClient(postmarkApiToken);
-            Message message = new Message(senderEmail, emailDetail.getAccount().getEmail(), emailDetail.getSubject(), template);
-            message.setMessageStream("transactional");
-            MessageResponse response = client.deliverMessage(message);
-            if (response.getErrorCode() == 0) {
-                logger.info("Send verify email to: {}", emailDetail.getAccount().getEmail());
-                return true;
-            } else {
-                logger.error("Postmark error sending verify email: {} - {}", response.getErrorCode(), response.getMessage());
-                throw new EmailException("Error sending email: " + response.getMessage(), HttpStatus.BAD_REQUEST);
-            }
-        } catch (EmailException e) {
-            throw e;
-        } catch (Exception e) {
-            logger.error("Unexpected error sending verify email: {}", e.getMessage(), e);
-            throw new EmailException("Unexpected error when sending email", HttpStatus.INTERNAL_SERVER_ERROR);
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper messageHelper = new MimeMessageHelper(message);
+            messageHelper.setFrom("globalskillsexe@gmail.com");
+            messageHelper.setTo(emailDetail.getAccount().getEmail());
+            messageHelper.setText(template,true);
+            messageHelper.setSubject(emailDetail.getSubject());
+            javaMailSender.send(message);
+            return true;
+        }catch(MessagingException e){
+            throw new EmailException("Error sending  email, please check your mail again", HttpStatus.BAD_REQUEST);
         }
     }
 
@@ -73,21 +54,15 @@ public class EmailService {
             context.setVariable("link", emailDetail.getLink());
             context.setVariable("date", emailDetail.getCreatedDate());
             String template = templateEngine.process("EmailResetPassword", context);
-            ApiClient client = Postmark.getApiClient(postmarkApiToken);
-            Message message = new Message(senderEmail, emailDetail.getAccount().getEmail(), emailDetail.getSubject(), template);
-            message.setMessageStream("transactional");
-            MessageResponse response = client.deliverMessage(message);
-            if (response.getErrorCode() == 0) {
-                logger.info("Send verify email to: {}", emailDetail.getAccount().getEmail());
-            } else {
-                logger.error("Postmark error sending verify email: {} - {}", response.getErrorCode(), response.getMessage());
-                throw new EmailException("Error sending email: " + response.getMessage(), HttpStatus.BAD_REQUEST);
-            }
-        } catch (EmailException e) {
-            throw e;
-        } catch (Exception e) {
-            logger.error("Unexpected error sending verify email: {}", e.getMessage(), e);
-            throw new EmailException("Unexpected error when sending email", HttpStatus.INTERNAL_SERVER_ERROR);
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper messageHelper = new MimeMessageHelper(message);
+            messageHelper.setFrom("admin@gmail.com");
+            messageHelper.setTo(emailDetail.getAccount().getEmail());
+            messageHelper.setText(template,true);
+            messageHelper.setSubject(emailDetail.getSubject());
+            javaMailSender.send(message);
+        }catch(MessagingException e){
+            throw new EmailException("Error sending  email, please check your mail again", HttpStatus.BAD_REQUEST);
         }
     }
 
@@ -99,21 +74,15 @@ public class EmailService {
             context.setVariable("link", emailDetail.getLink());
             context.setVariable("date", emailDetail.getCreatedDate());
             String template = templateEngine.process("EmailApproveCV", context);
-            ApiClient client = Postmark.getApiClient(postmarkApiToken);
-            Message message = new Message(senderEmail, emailDetail.getAccount().getEmail(), emailDetail.getSubject(), template);
-            message.setMessageStream("transactional");
-            MessageResponse response = client.deliverMessage(message);
-            if (response.getErrorCode() == 0) {
-                logger.info("Send verify email to: {}", emailDetail.getAccount().getEmail());
-            } else {
-                logger.error("Postmark error sending verify email: {} - {}", response.getErrorCode(), response.getMessage());
-                throw new EmailException("Error sending email: " + response.getMessage(), HttpStatus.BAD_REQUEST);
-            }
-        } catch (EmailException e) {
-            throw e;
-        } catch (Exception e) {
-            logger.error("Unexpected error sending verify email: {}", e.getMessage(), e);
-            throw new EmailException("Unexpected error when sending email", HttpStatus.INTERNAL_SERVER_ERROR);
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper messageHelper = new MimeMessageHelper(message);
+            messageHelper.setFrom("admin@gmail.com");
+            messageHelper.setTo(emailDetail.getAccount().getEmail());
+            messageHelper.setText(template,true);
+            messageHelper.setSubject(emailDetail.getSubject());
+            javaMailSender.send(message);
+        }catch(MessagingException e){
+            throw new EmailException("Error sending  email, please check your mail again", HttpStatus.BAD_REQUEST);
         }
     }
 
@@ -125,21 +94,15 @@ public class EmailService {
             context.setVariable("link", emailDetail.getLink());
             context.setVariable("date", emailDetail.getCreatedDate());
             String template = templateEngine.process("EmailRejectCV", context);
-            ApiClient client = Postmark.getApiClient(postmarkApiToken);
-            Message message = new Message(senderEmail, emailDetail.getAccount().getEmail(), emailDetail.getSubject(), template);
-            message.setMessageStream("transactional");
-            MessageResponse response = client.deliverMessage(message);
-            if (response.getErrorCode() == 0) {
-                logger.info("Send verify email to: {}", emailDetail.getAccount().getEmail());
-            } else {
-                logger.error("Postmark error sending verify email: {} - {}", response.getErrorCode(), response.getMessage());
-                throw new EmailException("Error sending email: " + response.getMessage(), HttpStatus.BAD_REQUEST);
-            }
-        } catch (EmailException e) {
-            throw e;
-        } catch (Exception e) {
-            logger.error("Unexpected error sending verify email: {}", e.getMessage(), e);
-            throw new EmailException("Unexpected error when sending email", HttpStatus.INTERNAL_SERVER_ERROR);
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper messageHelper = new MimeMessageHelper(message);
+            messageHelper.setFrom("admin@gmail.com");
+            messageHelper.setTo(emailDetail.getAccount().getEmail());
+            messageHelper.setText(template,true);
+            messageHelper.setSubject(emailDetail.getSubject());
+            javaMailSender.send(message);
+        }catch(MessagingException e){
+            throw new EmailException("Error sending  email, please check your mail again", HttpStatus.BAD_REQUEST);
         }
     }
 }
